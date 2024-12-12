3ca4f0fdfa41912dfb14a8c0a953d9bf9e61654c
akshaya-87 <akshaya061998@gmail.com>
2024-12-12 18:13:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-# app/main.py
-def greet():
-    return "Hello, World!"
+# app.py
 
-if __name__ == "__main__":
-    print(greet())
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello, World!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
